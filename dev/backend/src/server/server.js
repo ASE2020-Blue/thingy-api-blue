ddfbdb6ba6d5815c7501b5b8f48fdadb75cbcf05
Yael <yael.iseli@gmail.com>
2020-12-13 18:44:42 +0100

    #105: implement nginx WS and create prod docker environment

--- dev/backend/src/server/server.js
+++ dev/backend/src/server/server.js
@@ -1,7 +1,7 @@
-const Koa = require('koa');
-const router = require('koa-router')();
-const bodyParser = require('koa-bodyparser');
-const cors = require('@koa/cors');
+const Koa = require("koa");
+const router = require("koa-router")();
+const bodyParser = require("koa-bodyparser");
+const cors = require("@koa/cors");
 
 const thingies = require("../routes/thingies");
 const historyLocations = require("../routes/locationHistories");
@@ -12,7 +12,7 @@ const app = new Koa();
 // logger
 app.use(async (ctx, next) => {
   await next();
-  const rt = ctx.response.get('X-Response-Time');
+  const rt = ctx.response.get("X-Response-Time");
   console.log(`${ctx.method} ${ctx.url} - ${rt}`);
 });
 
@@ -21,14 +21,19 @@ app.use(async (ctx, next) => {
   const start = Date.now();
   await next();
   const ms = Date.now() - start;
-  ctx.set('X-Response-Time', `${ms}ms`);
+  ctx.set("X-Response-Time", `${ms}ms`);
 });
+
+const options = {
+  credentials: true,
+};
+
+if (process.env.NODE_ENV === "production")
+  options.origin = process.env.APP_BASE_URL;
 
 app
   .use(bodyParser())
-  .use(cors({
-    credentials: true
-  }))
+  .use(cors(options))
   .use(router.routes())
   .use(router.allowedMethods())
   .use(thingies.routes())
