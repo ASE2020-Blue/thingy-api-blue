d534422a9fc844d22d6ccb85ed2e08512a482544
Yael <yael.iseli@gmail.com>
2020-12-14 20:43:10 +0100

    #113: fix env variable for base url

--- dev/frontend/src/api/repository.js
+++ dev/frontend/src/api/repository.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-// TODO: change
-const baseURL = "http://51.15.209.246"; // process.env.VUE_APP_API_BASE_URL;
-// const baseURL = "http://localhost:8080"; // process.env.VUE_APP_API_BASE_URL;
+const baseURL = process.env.VUE_APP_API_BASE_URL;
 axios.defaults.withCredentials = true;
 
 const httpClient = axios.create({
