d39653ddbe64534c4146fbb2e18fcf33f391137b
Alain Schaller <schallerala@gmail.com>
2020-12-15 22:00:48 +0100

    #121: Fix enum of ints into the proper string

--- dev/backend/src/services/server/ThingyServer.js
+++ dev/backend/src/services/server/ThingyServer.js
@@ -3,6 +3,7 @@ const { Empty } = require('google-protobuf/google/protobuf/empty_pb');
 
 const { createEnvValue } = require("../../controllers/thingy");
 const { Thingy, LocationHistory } = require("../../models");
+const { ThingyValue } = require("../../proto/thingy_pb");
 const { IThingyPersistenceServer } = require('../../proto/thingy_grpc_pb');
 
 class ThingyServer /** @implements IThingyPersistenceServer */ {
@@ -65,7 +66,7 @@ class ThingyServer /** @implements IThingyPersistenceServer */ {
     setNewValue(call /** @type ServerUnaryCall<ThingyValue, Empty> */, callback /** @type sendUnaryData<Empty> */) {
         console.log(`${new Date().toISOString()}    setNewValue`);
         const thingyUuid = call.request.getThingyUuid();
-        const envParam = call.request.getEnvParam();
+        const envParam = this.convertThingyEnvParam(call.request.getEnvParam());
         const value = call.request.getValue();
         console.log(`\t${thingyUuid}[${envParam}]: ${value}`);
 
@@ -76,6 +77,16 @@ class ThingyServer /** @implements IThingyPersistenceServer */ {
 
         createEnvValue(thingyUuid, envParam, value)
             .then(() => callback(null, new Empty()), error => callback(error));
+    }
+
+    /**
+     * @param {Number} envParamIndex int representation of the value of the enum
+     * @return {String} the name of the value
+     */
+    convertThingyEnvParam (envParamIndex) {
+        const { EnvParam } = ThingyValue;
+        const keys = Object.keys(EnvParam);
+        return keys[envParamIndex];
     }
 }
 
