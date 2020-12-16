6f3a43057e433610913a6b739d97ba155b8f2f05
Alain Schaller <schallerala@gmail.com>
2020-12-16 01:35:37 +0100

    #122: Specify docker compose file in init script

--- dev/docker/nginx/init-letsencrypt.sh
+++ dev/docker/nginx/init-letsencrypt.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-# Get out of docker/nginx folder
-cd ../..
-
 if ! [ -x "$(command -v docker-compose)" ]; then
   echo 'Error: docker-compose is not installed.' >&2
   exit 1
 fi
 
 domains=(thingy-blue.xyz)
+docker_compose_file="docker-compose.prod.yml"
 rsa_key_size=4096
 data_path="./docker/nginx/certbot"
 email="" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+
+if ! [ -e $docker_compose_file ]; then
+  >&2 echo "docker_compose file not existed"
+  exit 1
+else if ! docker_compose -f $docker_compose_file config > /dev/null; then
+  >&2 echo "Failed to read the configuration of the docker compose"
+  exit 1
+fi
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
@@ -33,7 +39,7 @@ fi
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
-docker-compose run --rm --entrypoint "\
+docker-compose -f $docker_compose_file run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
@@ -42,11 +48,11 @@ echo
 
 
 echo "### Starting nginx ..."
-docker-compose up --force-recreate -d nginx
+docker-compose -f $docker_compose_file up --force-recreate -d nginx
 echo
 
 echo "### Deleting dummy certificate for $domains ..."
-docker-compose run --rm --entrypoint "\
+docker-compose -f $docker_compose_file run --rm --entrypoint "\
   rm -Rf /etc/letsencrypt/live/$domains && \
   rm -Rf /etc/letsencrypt/archive/$domains && \
   rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
@@ -69,7 +75,7 @@ esac
 # Enable staging mode if needed
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
-docker-compose run --rm --entrypoint "\
+docker-compose -f $docker_compose_file run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
     $email_arg \
@@ -80,4 +86,4 @@ docker-compose run --rm --entrypoint "\
 echo
 
 echo "### Reloading nginx ..."
-docker-compose exec nginx nginx -s reload
+docker-compose -f $docker_compose_file exec nginx nginx -s reload
